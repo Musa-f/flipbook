44a211a3a4a3275774e2971b014c9b207b9c041e
musa-f <musaevafatima77@gmail.com>
2023-01-18 09:06:26 +0100

    change

--- bdd.php
+++ bdd.php
@@ -4,12 +4,6 @@
     $username = 'root';
     $password = '';
 
-    //$bdd = mysqli_connect($servername, $username, $password, "biblio");
-    //if(!$bdd) die('Erreur:'.mysqli_connect_error());
-
-    $bdd = new PDO("mysql:host=$servername; dbname=biblio", $username, $password);
-
-    $test = "UPDATE users SET loginn='joe' WHERE id=1";
-    $bdd->exec($test);
+    $bdd = new PDO("mysql:host=$servername; dbname=flipbook", $username, $password);
 
 ?>
